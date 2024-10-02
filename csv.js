9909935a209f5be3b023650c1a600541b8e28db0
RodiaThomas <rodiathomas@itis-molinari.eu>
2024-10-02 13:09:24 +0200

    Third comit

--- csv.js
+++ csv.js
@@ -1,18 +1,18 @@
 let csv="nome,cognome,italiano,storia,matematica,informatica,inglese,tpsi,gmpoi\nMario,Rossi,6,7,6,6,5,6,6\nSandra,Verdi,5,5,7,7,10,8,7"
 let convert=(stringa)=>{
-let rows=riga.split("\n");
+let rows=stringa.split("\n");
 let l=rows.length;
 let strfin="nome,cognome,media,status,Insufficienti\n";
 
 
 for(let i=1;i<l;i++){
 let val=rows[i].split(",");
-let media;
-let insufficenze=0;
-for(let j=2;j<val.length;i++){
-    media+=val[j];
-    if(val[j]<6){
-        insufficenze++;
+let media=0;
+let insufficienze=0;
+for(let j=2;j<val.length;j++){
+    media+=parseInt(val[j]);
+    if(parseInt(val[j])<6){
+        insufficienze++;
     }
 }
 media=media/(val.length-2);
@@ -20,7 +20,7 @@ let status="Sufficiente"
 if(media<6){
     status="Insufficiente"
 }
-let row_to_insert= val[0]+val[1]+media+status+insufficenze+"\n";
+let row_to_insert= val[0]+","+val[1]+","+media+","+status+","+insufficienze+"\n";
 strfin+=row_to_insert;
 }
 return strfin;
